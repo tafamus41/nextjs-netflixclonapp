5c01e157fec2dcac85bcb5f87072658cf2b32c17
asimov <seyranmustafa4@gmail.com>
2024-11-18 02:45:39 +0300

    Navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 
+import Navbar from "@/components/Navbar";
 import "./globals.css";
 
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Navbar/>
         {children}
       </body>
     </html>
