333d7b7218534db4c6edf44f6f2357db40dc00f3
asimov <seyranmustafa4@gmail.com>
2024-11-18 03:25:14 +0300

    home page

---
+++ src/app/(private)/profile/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+export const metadata = {
+  title: "Profile",
+};
+const Profile = () => {
+  return (
+    <div>Profile</div>
+  )
+}
+
+export default Profile
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <h1>HOME</h1>
+    <div className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-fixed bg-no-repeat bg-center bg-cover">
+      <div className="bg-black w-full h-full bg-opacity-50">
+        <div className="relative top-2/4 m-auto text-white text-center">
+          <h1 className="text-5xl font-[900] mb-3">
+            Unlimited movies, TV shows, and more
+          </h1>
+          <p className="text-2xl font-[400]">Watch anywhere.Cancel anytime</p>
+          <Link href="/register">
+            <button className="btn-danger w-[250px]">Get Started</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
