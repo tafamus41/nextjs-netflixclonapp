f651ff836ace36e5f702937201488a6924d9eb79
asimov <seyranmustafa4@gmail.com>
2024-11-20 05:33:40 +0300

    profile

--- src/app/(private)/profile/page.jsx
+++ src/app/(private)/profile/page.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import CardContainer from "./components/CardContainer";
 export const metadata = {
-  title: "Profile",
+  title: "Profile Page",
 };
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="flex items-center justify-center pt-20">
+      <div className="flex flex-col">
+        <h1 className="text-3xl md:text-6xl text-white text-center">
+          Who's watching?
+        </h1>
+        <CardContainer />
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
