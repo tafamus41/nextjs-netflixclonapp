75ab7ceb1b2597a71aad8a6178e0a4e2fa746755
asimov <seyranmustafa4@gmail.com>
2024-11-20 16:01:14 +0300

    moviefunctions

--- src/app/(private)/movies/page.jsx
+++ src/app/(private)/movies/page.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import { getMovies } from "@/helpers/movieFunctions";
+import React from "react";
+import HeroSection from "./components/HeroSection";
+import MovieSection from "./components/MovieSection";
 
-const Movies = ({params}) => {
-  const {movieId}=params;
+export const metadata = {
+  title: "Movies Page",
+};
+const Movies = async () => {
+  const movies = await getMovies("now_playing");
+  // console.log(movies);
   return (
-    <div>Movies</div>
-  )
-}
+    <div>
+      <HeroSection
+        title={movies[0]?.title}
+        overview={movies[0]?.overview}
+        id={movies[0]?.id}
+      />
+      <MovieSection title="NOW PLAYING" type="now_playing" />
+      <MovieSection title="POPULAR" type="popular" />
+      <MovieSection title="TOP RATED" type="top_rated" />
+      <MovieSection title="UPCOMING" type="upcoming" />
+    </div>
+  );
+};
 
-export default Movies
+export default Movies;
